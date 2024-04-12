7154efaa3c8ed510813bbcfdf806fcf70bf78567
Juan El <ooze.orb@gmail.com>
2024-04-12 14:28:49 +0200

    :tada: very simple customer code

--- src/lambda/user_app/main.py
+++ src/lambda/user_app/main.py
@@ -2,4 +2,11 @@
 # It will contain the args of the emitted event in an array so [1n, 2n] - event["args"]
 
 def main(event, _):
-    print("Hello World!")
+    print("Hello World! and a simple sum")
+    a = 5
+    b = 7
+    result = a + b
+    print(f"The sum of {a} and {b} is {result}")
+    return result
+if __name__ == "__main__":
+    main(None, None)
