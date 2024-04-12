d2d2f58266a5c90c548d91dcba1f0348f52a9ca2
Quix <quixcodes@gmail.com>
2024-04-12 12:33:56 +0200

    Initial commit

---
+++ test/ethdam-2024.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ethdam2024 from '../lib/ethdam-2024-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ethdam-2024-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ethdam2024.Ethdam2024Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
