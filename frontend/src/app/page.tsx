8c292ae4e763efaed598881d50f14516712d1f5d
Juan el <ooze.orb@gmail.com>
2024-04-13 10:08:32 +0200

    added button and input field

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
 
 import Image from 'next/image';
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
@@ -15,9 +19,22 @@ export default function Home() {
           />
         </header>
       </div>
-      <div className="grid grid-cols-12 gap-4">
-        <div></div>  
-        <div></div>
+      <div className="grid grid-cols-12 gab-4"> 
+        <div className="col-span-4">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" />
+            </div>
+        </div>
+        <div className="col-span-3">
+          // Add event selection dropdown here
+        </div>
+        <div className="col-span-3">
+          // Add container selection dropdown here
+        </div>  
+        <div className="col-span-2">
+          <Button variant="outline">Run</Button>
+        </div>
       </div>
     </main>
   );
